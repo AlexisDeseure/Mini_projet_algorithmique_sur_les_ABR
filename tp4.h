e8db0110063fe311b93014838eba018d8eaedc8d
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2023-05-11 08:41:10 +0200

    structures

--- tp4.h
+++ tp4.h
@@ -1,4 +1,9 @@
 #ifndef TP4_TP4_H
 #define TP4_TP4_H
-
+typedef struct Position{
+    int numeroLigne;
+    int ordre;
+    int numeroPhrase;
+    struct Position* suivant;
+}T_Position;
 #endif //TP4_TP4_H
